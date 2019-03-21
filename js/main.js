71a527b5d4e6ee19086d0f5f707df4431df419bc
Joscha Henningsen <44805696+joschahenningsen@users.noreply.github.com>
2019-03-22 00:42:39 +0100

    CSS updates

---
+++ js/main.js
@@ -0,0 +1,3 @@
+function clicked(clicked) {
+    alert(clicked);
+}
--- src/Server/Config.java
+++ src/Server/Config.java
@@ -24,6 +24,7 @@ public class Config {
                 "    <script src=\"https://code.jquery.com/jquery-3.3.1.slim.min.js\"></script>\n" +
                 "    <script src=\"https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js\"></script>\n" +
                 "    <script src=\"https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js\"></script>\n" +
+                "    <script src=\"js/main.js\"></script>\n" +
                 "    <meta charset=\"UTF-8\">\n" +
                 "    <link rel=\"stylesheet\" href=\"css/main.css\">");
 
